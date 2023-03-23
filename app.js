bc721146954e63a7039a7f31660913d5148d8a7a
Zakhiel50 <lucrigaud50@gmail.com>
2023-03-23 12:36:46 +0100

    create in file app.js: deckPlayer for store the choosen card + reserve to store cards + deckPLayer to contain deckPlayer and reserve + add their classes in css + various css patches

---
+++ app.js
@@ -0,0 +1,45 @@
+const bulbasaur = document.querySelector(".bulbasaur");
+const charmander = document.querySelector(".charmander");
+const squirtle = document.querySelector(".squirtle");
+const container = document.querySelector("#window")
+
+const sectionPlayer = document.createElement("section")
+container.appendChild(sectionPlayer)
+sectionPlayer.classList = "section-player"
+
+
+
+
+
+
+bulbasaur.addEventListener("click", function() {
+
+    const deckPlayer = document.createElement("div")
+sectionPlayer.appendChild(deckPlayer)
+deckPlayer.classList = "deck-player"
+
+const reserve = document.createElement("div")
+sectionPlayer.appendChild(reserve)
+reserve.classList = "reserve"
+
+console.log(sectionPlayer);
+let test = bulbasaur.cloneNode(true)
+
+    container.style.flexDirection = "column"
+
+    const containerCards = document.querySelector("#container-cards")
+    containerCards.remove()
+    const header = document.querySelector("header")
+    header.remove()
+
+    reserve.appendChild(charmander);
+    reserve.appendChild(squirtle);
+
+    deckPlayer.appendChild(bulbasaur)
+    deckPlayer.classList.add("first");
+
+    bulbasaur.classList.add("pokemon-first")
+
+    const sectionBot = document.querySelector(".section-bot")
+    sectionBot.classList.remove("section-bot-hidden")
+})
